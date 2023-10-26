c9f8401ae8f1b265aba2fd4d088f887db6afa074
AllieStewart <megaxis1217@gmail.com>
2023-10-26 18:47:46 -0500

    removed index.js in routes, fixed connection issue with html.js to server.js, now must fix the note-saving issue

--- Develop/routes/html.js
+++ Develop/routes/html.js
@@ -3,14 +3,19 @@
 const html = require('express').Router();
 const path = require('path');
 
+// GET Route for homepage
+html.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '../public/index.html'))
+);
+
 // GET /notes should return the notes.html file.
 html.get('/notes', (req, res) =>
-    res.sendFile(path.join(__dirname, '/public/notes.html'))
+    res.sendFile(path.join(__dirname, '../public/notes.html'))
 );
 
 // GET * should return the index.html file.
 html.get('*', (req, res) =>
-    res.sendFile(path.join(__dirname, '/public/index.html'))
+    res.sendFile(path.join(__dirname, '../public/index.html'))
 );
 
 module.exports = html;
--- Develop/server.js
+++ Develop/server.js
@@ -1,23 +1,26 @@
 // Start of JS file
 // Hosts the site, retrieves info, sends info.
 const express = require('express');
-const path = require('path');
-const main = require('./routes/index');
+//const path = require('path');
+const { clog } = require('./helpers/clog');
+//const main = require('./routes/index');
+const html = require('./routes/html');
+const api = require('./routes/api');
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+app.use(clog);
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/api', main);
-
 app.use(express.static('public'));
 
-// GET Route for homepage
-// app.get('/', (req, res) =>
-//   res.sendFile(path.join(__dirname, '/public/index.html'))
-// );
+app.use(html);
+app.use(api);
+
+// app.use('/api', main);
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT}`)
