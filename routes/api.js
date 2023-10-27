c0394abd6067bb155c1c6151348824976665e6bd
AllieStewart <megaxis1217@gmail.com>
2023-10-27 11:27:45 -0500

    fixed delete route in api.js, removed funky function, added send

--- routes/api.js
+++ routes/api.js
@@ -48,17 +48,17 @@ api.post('/api/notes', (req, res) =>
 });
 
 // DELETE route for note
-// api.delete('/api/notes/:id', (req, res) => {
-//     const notesId = req.params.id;
-//     readFromFile('./db/db.json')
-//     .then((data) => JSON.parse(data))
-//     .then((json) => 
-//     {
-//         const result = json.filter((text) => text.id != notesId);
-//         writeToFile('./db/db.json', result);
-//         res.json(`Item ${id} has been deleted 🗑️`);
-//     })
-// });
+api.delete('/api/notes/:id', (req, res) => {
+    const notesId = req.params.id;
+    readFromFile('./db/db.json')
+    .then((data) => JSON.parse(data))
+    .then((json) => 
+    {
+        const result = json.filter((text) => text.id != notesId);
+        writeToFile('./db/db.json', result);
+    });
+    res.send(`Deleted note with ${req.params.id}`);
+});
 
 module.exports = api;
 // End of JS file
